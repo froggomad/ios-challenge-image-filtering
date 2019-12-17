10987f447e674e793c7c260721411e1c92836f19
Spencer Curtis <spencercolecurtis@gmail.com>
2019-12-17 15:43:54 -0700

    Add starter project

---
+++ ImageFiltering/FilteringViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ImageFiltering
+//
+//  Created by Spencer Curtis on 12/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class FilteringViewController: UIViewController {
+    
+    @IBOutlet weak var inputImageView: UIImageView!
+    @IBOutlet weak var outputImageView: UIImageView!
+    
+    let imageFilterer = ImageFilterer()
+    
+    @IBAction func performFilter(_ sender: Any) {
+        
+    }
+}
+
---
+++ ImageFiltering/ImageFilterer.swift
@@ -0,0 +1,19 @@
+//
+//  ImageFilterer.swift
+//  ImageFilteringResourcefulnessAssessment
+//
+//  Created by Spencer Curtis on 12/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ImageFilterer {
+    
+    let context = CIContext(options: nil)
+    
+    func filterImage(_ image: UIImage?) -> UIImage? {
+        // Remove this `return nil` when you work on this function. This is just here to have the compiler not throw an error.
+        return nil
+    }
+}
