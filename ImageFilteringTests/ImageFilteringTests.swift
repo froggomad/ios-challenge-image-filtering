4c0f431a9a197bcbcb9b334f8de482292fb6a0bc
Andrew Madsen <andrew@openreelsoftware.com>
2019-12-18 15:15:48 -0700

    Simplified tests

--- ImageFilteringTests/ImageFilteringTests.swift
+++ ImageFilteringTests/ImageFilteringTests.swift
@@ -11,90 +11,33 @@ import XCTest
 
 class ImageFilteringTests: XCTestCase {
     
-    let imageFilterer = ImageFilterer()
-    let context = CIContext(options: nil)
-    
-    static var filteredImageData: Data?
-    
-    override class func setUp() {
-        
-        let semaphore = DispatchSemaphore(value: 0)
-        
-        // Fetch the pre-filtered image to use as a comparison to the filtered image from the student before beginning the test.
-        fetchFilteredImage(completion: { (data) in
-            
-            filteredImageData = data
-            
-            semaphore.signal()
-        })
-        
-        semaphore.wait()
-    }
-    
     func testImageFiltering() {
+        let imageFilterer = ImageFilterer()
         
-        guard let lionImage = UIImage(named: "Lion"),
-            let filteredImage = imageFilterer.filterImage(lionImage),
-            let base64FilteredImage = filteredImage.pngData()?.base64EncodedString(),
-            let base64ComparisonImage = ImageFilteringTests.filteredImageData?.base64EncodedString() else {
+        let bundle = Bundle(for: type(of: self))
+        
+        guard let lionImage = UIImage(named: "Lion", in: bundle, compatibleWith: nil),
+            let comparisonFilteredImage = filterImage(lionImage),
+            let filteredImage = imageFilterer.filterImage(lionImage) else {
                 XCTFail("Image(s) are nil")
                 return
         }
         
-        let distance = hammingDistance(photoOne: base64FilteredImage, photoTwo: base64ComparisonImage)
-        
-        XCTAssertEqual(distance, 0)
+        XCTAssertEqual(comparisonFilteredImage.pngData(), filteredImage.pngData())
     }
     
     private func filterImage(_ image: UIImage?) -> UIImage? {
-        
-        let filter = CIFilter(name: "CIPhotoEffectInstant")!
-        
         guard let image = image else { return nil }
         
         let ciImage = CIImage(image: image)
+        let filter = CIFilter(name: "CIPhotoEffectInstant")!
+        filter.setValue(ciImage, forKey: kCIInputImageKey)
         
-        filter.setValue(ciImage, forKey: "inputImage")
-        
-        var filteredImage: UIImage? = nil
-        
-        if let outputImage = filter.outputImage,
-            
-            let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
-            
-            filteredImage = UIImage(cgImage: cgImage)
+        let context = CIContext(options: nil)
+        guard let outputImage = filter.outputImage,
+            let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {
+                return nil
         }
-        
-        return filteredImage
-    }
-    
-    private func hammingDistance(photoOne: String, photoTwo: String) -> Int? {
-        guard !photoOne.isEmpty, !photoTwo.isEmpty else {
-            return nil
-        }
-        
-        var w1Iterator = photoOne.makeIterator()
-        var w2Iterator = photoTwo.makeIterator()
-        
-        var distance = 0;
-        while let w1Char = w1Iterator.next(), let w2Char = w2Iterator.next()  {
-            distance += (w1Char != w2Char) ? 1 : 0
-        }
-        return distance
-    }
-    
-    static private func fetchFilteredImage(completion: @escaping (Data?) -> Void) {
-        
-        let imageURL = URL(string: "https://tk-assets.lambdaschool.com/d6b9da5c-24a3-43e0-96d1-3ffe83dc8161_lion.png")!
-        
-        URLSession.shared.dataTask(with: imageURL) { (data, _, error) in
-            
-            if let error = error {
-                NSLog("Error fetching filtered image to compare: \(error)")
-            }
-            
-            completion(data)
-            
-        }.resume()
+        return UIImage(cgImage: cgImage)
     }
 }
